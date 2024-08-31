93e047a557474cbcb000f9d2c9e582143edbb0bd
Junaid Hassan <junaid.hassan@evampsaanga.com>
2024-08-31 11:01:21 +0500

    update for network layer status codes

--- lib/network/connectionmanager/network_connection_manager_helper_util.dart
+++ lib/network/connectionmanager/network_connection_manager_helper_util.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 import 'package:network_utils_pack/network/util/imports_util.dart';
 
 class NetworkConnectionManagerHelperUtil {
@@ -9,10 +12,33 @@ class NetworkConnectionManagerHelperUtil {
 
   ///To check if the network connection is available
   Future<bool> isNetworkAvailable() async {
+    if(kIsWeb){
+      return await _webHasInternet();
+    }else{
+      return await _mobileHasInternet();
+    }
+  }
+
+  Future<bool> _mobileHasInternet()async{
     bool isNetworkAvailable = await InternetConnectionChecker().hasConnection;
     if (isNetworkAvailable) {
       return true;
     } else {
+      return false;
+    }
+  }
+
+  Future<bool> _webHasInternet() async {
+    try {
+      final result = await http.get(Uri.parse('www.google.com'));
+      if(result.statusCode==200){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    on SocketException catch (_) {
       return false;
     }
   }
